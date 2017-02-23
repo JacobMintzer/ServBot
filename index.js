0592cf1989fbadbe7ee52303f232c3dd0156597e
Jacob Mintzer <jrmintz3@gmail.com>
2017-02-23 17:37:55 -0500

    first new functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Discord = require('discord.io');
 const config=require('./config.json');
+const prefix='servbot ';
 var bot = new Discord.Client({
 	autorun: true,
 	token: config.token
@@ -9,9 +10,20 @@ bot.on('ready',function(event){
 });
 bot.on('message', function(user,userID,channelID, message, event){
 	if(message==="ping"){
-		bot.sendMessage({
-			to:channelID,
-			message: "I am dead inside Miss Tron"
+			bot.sendMessage({
+				to:channelID,
+				message: "I am dead inside Miss Tron"
+				});
+	}
+	if(message.startsWith(prefix)){
+		message = message.replace(config.prefix,'');
+		if(message.startsWith("sad")){
+			bot.sendMessage({
+				to:channelID,
+				message: "http://wiinoob.walyou.netdna-cdn.com/wp-content/uploads/2011/07/servbot-crying-e1311106058106.png"
 			});
 		}
+		
+		
+	}
 });
